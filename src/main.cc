8dc3952f91f6da91591b10bdbf79792021a2f27e
cedric <cedric.parpet@orange.fr>
2021-11-07 15:51:05 +0100

    Add a simple Map class

---
+++ src/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "map.h"
+
+using namespace std;
+
+int main() {
+    Map map;
+    map.display();
+
+    return 0;
+
+}
---
+++ src/map.cc
@@ -0,0 +1,7 @@
+#include "map.h"
+#include <iostream>
+
+using namespace std;
+void Map::display() {
+    cout << "hi :D I'm the map" << endl;
+}
