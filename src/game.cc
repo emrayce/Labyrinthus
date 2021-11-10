b2a730bbaa9e32d3b5532c5087aa455953721b8e
cedric <cedric.parpet@orange.fr>
2021-11-11 00:17:39 +0100

    Add class to manage game loop

---
+++ src/game.cc
@@ -0,0 +1,56 @@
+#include "game.h"
+
+#include <cstddef>
+#include <utility>
+#include <iostream>
+
+using namespace std;
+
+Game::Game() {
+    map = new Map();
+}
+
+void Game::movePlayer(int x, int y) {
+#include "player.h"
+    size_t newPosX = map->getPlayerPosition().first + x;
+    size_t newPosY = map->getPlayerPosition().second + y;
+
+    if (map->isPositionValid(newPosX, newPosY)) {
+        map->movePlayer(newPosX, newPosY);
+    }
+}
+
+// the game loop where input are taken and game is played
+void Game::gameLoop() {
+    map->display();
+    bool win = false;
+    while (!win) {
+        char input = ' ';
+
+        cin >> input;
+
+        switch (input) {
+            case 'w':
+                movePlayer(-1, 0);
+                break;
+            case 's':
+                movePlayer(1, 0);
+                break;
+            case 'a':
+                movePlayer(0, -1);
+                break;
+            case 'd':
+                movePlayer(0, 1);
+                break;
+        }
+
+        
+        if (map->getPlayerPosition() == map->getEndPosition()) {
+            win = true;
+            break;
+        }
+
+        map->display();
+    }
+
+}
