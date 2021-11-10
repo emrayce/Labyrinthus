a1a122a7fdb17ae2ff0d067cf633f44dadb182ec
cedric <cedric.parpet@orange.fr>
2021-11-11 00:31:37 +0100

    Clear screen before displaying map

--- src/map.cc
+++ src/map.cc
@@ -7,6 +7,9 @@
 using namespace std;
 
 void Map::display() {
+    //clear screen and set curso to row1 col1
+    cout << "\033[2J\033[1;1H";
+
     string displayed = "";
     for (size_t i = 0; i < SIZE; i++) {
         for (size_t j = 0; j < SIZE; j++) {
