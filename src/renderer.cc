7a27b10b16cbab0d2fa6e1eb9416d969202c4288
cedric <cedric.parpet@orange.fr>
2021-12-06 02:31:08 +0100

    Add class Renderer and its methods to create pixels and render

---
+++ src/renderer.cc
@@ -0,0 +1,106 @@
+#include "renderer.h"
+
+#include <iostream>
+
+using namespace std;
+
+string Renderer::createPixel(char c) {
+    string pixel = "";
+    switch (c) {
+        case '#':
+            pixel += "22-0-0";
+            break;
+        case ' ':
+            pixel += "209-0-0";
+            break;
+        default:
+            pixel += "255-0-0";
+    }
+
+    return pixel;
+}
+
+void Renderer::setScreenFromMapCell(MapCell mapCell) {
+    for (size_t row = 0; row < WIDTH; row++) {
+        for (size_t col = 0; col < LENGTH; col++) {
+            string pixel = createPixel(mapCell.getChar(row, col));
+            setPixel(row, col, pixel);
+        }
+    }
+}
+
+void Renderer::setPixel(size_t row, size_t col, string pixel) {
+    if (row < WIDTH && col <= LENGTH) {
+        screen.at(row * LENGTH + col) = pixel;
+    }
+}
+
+string Renderer::getPixel(size_t row, size_t col) {
+    return screen.at(row * LENGTH + col);
+}
+
+
+string Renderer::getBackgroundColor(string pixel) {
+    size_t pos = pixel.find("-");
+
+    string background = pixel.substr(0, pos - 1);
+    //cout << background << endl;
+
+    return background;
+}
+
+string Renderer::getForeGroundColor(string pixel) {
+    size_t pos1 = pixel.find("-");
+    size_t pos2 = pixel.rfind("-");
+
+    string foreground = pixel.substr(pos1 + 1, pos2 - pos1 - 1);
+    //cout << foreground << endl;
+
+    return foreground;
+}
+
+string Renderer::getAsciiChar(string pixel) {
+    size_t pos = pixel.rfind("-");
+
+    string ascii = pixel.substr(pos + 1);
+    //cout << ascii << endl;
+
+    return ascii;
+}
+
+string Renderer::setBgColor(string colorID) {
+    string bgColor = "";
+    bgColor += ESC + "48;5;" + colorID + "m";
+
+    return bgColor;
+}
+
+string Renderer::setFgColor(string colorID) {
+    string fgColor = "";
+    fgColor += ESC + "38;5;" + colorID + "m";
+
+    return fgColor;
+}
+
+void Renderer::displayLine(size_t row) {
+    string to_display = "";
+    to_display += ESC + to_string(row) + ";OH";                  // set the cursor at the start of the row
+
+    for (size_t col = 0; col < LENGTH; col++) {
+        string bgColor = getBackgroundColor(getPixel(row, col));
+        string fgColor = getBackgroundColor(getPixel(row, col));
+        string ascii = getAsciiChar(getPixel(row, col));
+
+        to_display += setBgColor(bgColor);
+        to_display += setFgColor(fgColor);
+        to_display += ascii;
+    }
+
+    cout << to_display;
+}
+
+void Renderer::display() {
+    for (size_t row = 0; row < WIDTH; row++) {
+        displayLine(row);
+    }
+}
---
+++ src/renderer.h
@@ -0,0 +1,45 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include "map_cell.h"
+
+#include <iostream>
+
+using namespace std;
+
+class Renderer {
+    private:
+        static const size_t WIDTH = 20;
+        static const size_t LENGTH = 55;
+
+        string ESC = "\0x1b[";
+
+        // matrix of all pixels that will be display to the terminals
+        // A "pixel" (case of the array) will contain a string in the following format
+        // 255-255-255
+        // background color - foreground color - ascii character
+
+        array<string, LENGTH * WIDTH> screen;
+
+        string getBackgroundColor(string pixel);
+        string getForeGroundColor(string pixel);
+        string getAsciiChar(string pixel);
+
+        string setBgColor(string);
+        string setFgColor(string);
+
+    public:
+        Renderer();
+
+        // Getter/Setter of pixel of screen
+        void setPixel(size_t row, size_t col, string pixel);
+        string getPixel(size_t row, size_t col);
+
+        void setScreenFromMapCell(MapCell mapCell);
+        string createPixel(char c);
+
+        void displayLine(size_t row);
+        void display();
+};
+
+#endif
